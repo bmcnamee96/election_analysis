60f916e7e501c69cbae33a4304f333d8e6811c16
bmcnamee96 <bmac965@gmail.com>
2022-03-12 16:59:29 -0500

    found the winning candidate

--- PyPoll.py
+++ PyPoll.py
@@ -18,6 +18,11 @@ candidate_options = []
 # make a dictionary for the candidate votes
 candidate_votes = {}
 
+# winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # open the file and read the file
 with open(file_path) as election_data:
 
@@ -58,21 +63,22 @@ for candidate_name in candidate_votes:
     vote_percentage = float(votes) / float(total_votes) * 100
 
     #print the candidate name and percentage of votes
-    print(f"{candidate_name}: received {round(vote_percentage, 2)}% of the votes.")
+    print(f"{candidate_name}: {round(vote_percentage, 1)}% ({votes:,})\n")
 
-# print the total votes variable
-print(total_votes)
+    # determine winning vote count and candidate
+    # determine if the votes are greater than the winning count
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # if true then set winning_count = votes and winning_percent = vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # set the winning_candidate equal to the candidates name
+        winning_candidate = candidate_name
 
-# print the candidate list
-print(candidate_options)
-
-# print the candidates votes
-print(candidate_votes)
-
-
-# The data we need to retrieve
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentage of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote
+# print out the winning candidate, vote count and percentage
+winning_candidate_summary = (
+    f"--------------------------\n" 
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count {winning_count:,}\n" 
+    f"Winning Percentage: {winning_percentage:.1f}%\n" 
+    f"--------------------------\n")
+print(winning_candidate_summary)
