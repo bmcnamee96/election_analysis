6850547327ce3d96aeace171430f41c90d156254
bmcnamee96 <bmac965@gmail.com>
2022-03-12 16:35:43 -0500

    found total number and percentage of votes for each candidate

--- PyPoll.py
+++ PyPoll.py
@@ -15,6 +15,9 @@ total_votes = 0
 # make a candidate options list
 candidate_options = []
 
+# make a dictionary for the candidate votes
+candidate_votes = {}
+
 # open the file and read the file
 with open(file_path) as election_data:
 
@@ -34,9 +37,28 @@ with open(file_path) as election_data:
 
         # if the candidate does not match any existing candidate...
         if candidate_name not in candidate_options:
+            
             # add the name to the list
             candidate_options.append(candidate_name)
 
+            # track the candidates vote count
+            candidate_votes[candidate_name] = 0
+
+        # add 1 for every vote that the candidate gets
+        candidate_votes[candidate_name] += 1
+
+# determine the percentage of votes for each candidate by using a for loop
+# iterate through the candidate list
+for candidate_name in candidate_votes:
+    
+    # retrive vote count of a candidate
+    votes = candidate_votes[candidate_name]
+
+    # calculate the percentage of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    #print the candidate name and percentage of votes
+    print(f"{candidate_name}: received {round(vote_percentage, 2)}% of the votes.")
 
 # print the total votes variable
 print(total_votes)
@@ -44,7 +66,8 @@ print(total_votes)
 # print the candidate list
 print(candidate_options)
 
-
+# print the candidates votes
+print(candidate_votes)
 
 
 # The data we need to retrieve
